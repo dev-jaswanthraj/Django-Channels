3de29c497b4f76d810a117cfbc09a7c74eba484a
jaswanthraj <jaswanthraj.r@gmail.com>
2021-07-02 12:54:14 +0530

    Added all websocket Configuration

--- mysite/chat/urls.py
+++ mysite/chat/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
-    index
+    index,
+    room
 )
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<str:room_name>/', room, name='room'),
 ]
--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'chat/index.html')
+
+def room(request, room_name):
+    context = {
+        'room_name': room_name,
+    }
+    return render(request, 'chat/room.html', context)
